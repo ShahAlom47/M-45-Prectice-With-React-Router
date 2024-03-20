0d7c657cdce471c876f4c50d7030ba8366bf0f81
sah alom <sahalom4729@gmail.com>
2024-03-20 22:56:11 +0400

    add go back button

--- src/Components/ShowDetails.jsx
+++ src/Components/ShowDetails.jsx
@@ -1,12 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, useNavigate } from "react-router-dom";
 
 
 const ShowDetails = () => {
+    const navigate =useNavigate()
 
     const {meals} =useLoaderData();
    const datas= meals[0];
  const {strYoutube,strMeal,strMealThumb , strInstructions}=datas;
+ const backhndel =()=>{
+    navigate(-1)
+    }
+
     return (
+        <div>
         <div className="flex gap-4 w-10/12 m-auto p-7">
           <div className="w-4/12 bg-slate-200 p-4 rounded-lg ">
             <img
@@ -16,6 +22,10 @@ const ShowDetails = () => {
             <a  className="  flex items-center gap-1" target="blank"  href={strYoutube}> 
             <img className="w-[40px]" src="https://www.svgrepo.com/show/13671/youtube.svg" alt="" />
              <span className="text-lg font-semibold btn btn-link">Youtube </span></a>
+             <div className="flex justify-center py-6">
+            <button onClick={()=>backhndel()} className=" btn btn-outline  ">Go Back</button>
+
+             </div>
           </div>
           
           
@@ -23,6 +33,7 @@ const ShowDetails = () => {
           <h2 className="text-2xl mb-5 font-semibold">Instructions </h2>
           <p> {strInstructions}</p>
           </div>
+        </div>
         </div>
     );
 };
